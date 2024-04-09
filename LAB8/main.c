81cc07ccb982e2ebeb1e735a83eda8e8377ec189
Halil Bagosi <halilbagosi24@icloud.com>
2024-04-09 08:49:53 +0200

    Initial Commit

---
+++ LAB8/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LAB8
+//
+//  Created by Halil Bagosi on 9.4.24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
